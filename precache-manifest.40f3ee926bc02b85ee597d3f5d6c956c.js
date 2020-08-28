f2eff45a9abc43fabf52e443d54652385ae466b4
Ariminet <EmilAriminet@users.noreply.github.com>
2020-08-28 13:10:44 +0200

    deployed prod build

---
+++ precache-manifest.40f3ee926bc02b85ee597d3f5d6c956c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6848fb052125a5b3e2617fe683d50459",
+    "url": "/index.html"
+  },
+  {
+    "revision": "211a4a9a7700a0ddff16",
+    "url": "/static/css/main.2db71c46.chunk.css"
+  },
+  {
+    "revision": "dcb99abbce8b2a3b479e",
+    "url": "/static/js/2.c3992186.chunk.js"
+  },
+  {
+    "revision": "3205f2f97be94286aaab611b41fbc37d",
+    "url": "/static/js/2.c3992186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "211a4a9a7700a0ddff16",
+    "url": "/static/js/main.1c4c276d.chunk.js"
+  },
+  {
+    "revision": "d40dd99b75a2939b834d",
+    "url": "/static/js/runtime-main.80b024c4.js"
+  }
+]);
